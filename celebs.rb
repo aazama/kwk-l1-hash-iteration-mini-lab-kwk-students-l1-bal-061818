141be3d4cf74665b32f0f6084e957dc188b32209
ava azama <avaazama@gmail.com>
2018-06-20 20:13:14 +0000

    automatically backed up by learn

--- celebs.rb
+++ celebs.rb
@@ -1,4 +1,16 @@
 fav_celebrities = {}
-fav_celebrities= {"shay => "anthony bourdaine"}
-fav_celebrities ["Emma"]="Ed Sheeran"
+fav_celebrities= {"shay => "anthony bourdaine",
+  "Grace" => "Shawn Mendes",
+  "Lily" => "Orlando Bloom",
+  "Ava" => "Taylor Swift",
+  "Avani" => "Channing Tatum",
+  "Ume" => "Harry Styles",
+  "Ume" => "Harry Styles",
+   "Hadja" => "Rihanna",
+   "Anu" => "The Queen",
+   "Seema" => "Allison Kim",
+   "Jesse" => "Big Time Rush",
+   "Cassidy" => "Hannah Montana",
+fav_celebrities ["Emma"]="Ed Sheeran"}
+
 puts fav_celebrities
